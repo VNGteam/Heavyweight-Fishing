210ade644ee415fc6e5e5f7cc9117a4e7abd6fcc
Võ Nguyên Giáp <vonguyengiap@MacBook-Pro-cua-Vo.local>
2026-09-12 19:09:13 +0700

    fix: cap nhat badge commit 76b550e

--- local.lua
+++ local.lua
@@ -93,7 +93,7 @@ local activeConnections = {}
 local cleanUpInstances = {}
 
 --// MÃ COMMIT BẢN BUILD HIỆN TẠI (NHÚNG TĨNH TRONG CODE, KHÔNG DÙNG MẠNG) //--
-local SCRIPT_BUILD_COMMIT = "a40c045"
+local SCRIPT_BUILD_COMMIT = "76b550e"
 
 local Events = ReplicatedStorage:FindFirstChild("Events")
 if not Events then
