0124cbfb68c70dc7059da93820a95d6f9fa52059
Võ Nguyên Giáp <vonguyengiap@MacBook-Pro-cua-Vo.local>
2026-09-12 23:36:40 +0700

    chore: bump build commit to f497d7e

--- local.lua
+++ local.lua
@@ -93,7 +93,7 @@ local activeConnections = {}
 local cleanUpInstances = {}
 
 --// MÃ COMMIT BẢN BUILD HIỆN TẠI (NHÚNG TĨNH TRONG CODE, KHÔNG DÙNG MẠNG) //--
-local SCRIPT_BUILD_COMMIT = "1465362"
+local SCRIPT_BUILD_COMMIT = "f497d7e"
 
 local Events = ReplicatedStorage:FindFirstChild("Events")
 if not Events then
