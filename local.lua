3ab32de0c685801051e2516e43d1213577850b24
Võ Nguyên Giáp <vonguyengiap@MacBook-Pro-cua-Vo.local>
2026-09-12 18:12:34 +0700

    fix: cap nhat badge commit va hoan thien quet nhiem vu ve random

--- local.lua
+++ local.lua
@@ -93,7 +93,7 @@ local activeConnections = {}
 local cleanUpInstances = {}
 
 --// MÃ COMMIT BẢN BUILD HIỆN TẠI (NHÚNG TĨNH TRONG CODE, KHÔNG DÙNG MẠNG) //--
-local SCRIPT_BUILD_COMMIT = "76c7323"
+local SCRIPT_BUILD_COMMIT = "2fb1244"
 
 local Events = ReplicatedStorage:FindFirstChild("Events")
 if not Events then
